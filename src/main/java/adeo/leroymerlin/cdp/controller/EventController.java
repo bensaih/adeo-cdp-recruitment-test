20ea4ff9e00f1312c0d3929560d921dc8850e27b
ybensaih <bensaihyassine@gmail.com>
2021-05-09 16:19:01 +0200

    chore: remove unused dependency and clean code

--- src/main/java/adeo/leroymerlin/cdp/controller/EventController.java
+++ src/main/java/adeo/leroymerlin/cdp/controller/EventController.java
@@ -27,27 +27,27 @@ public class EventController implements EventApi {
 		this.eventMapper = eventMapper;
 	}
 
-	@GetMapping(value = GET_EVENTS_URL)
+	@GetMapping(GET_EVENTS_URL)
 	@Override
 	public List<EventViewModel> findEvents() {
 		return eventMapper.toEventViewModel(
 				eventService.getEvents());
 	}
 
-	@GetMapping(value = SEARCH_EVENTS_URL)
+	@GetMapping(SEARCH_EVENTS_URL)
 	@Override
 	public List<EventViewModel> findEvents(@PathVariable final String query) {
 		return eventMapper.toEventViewModel(
 				eventService.getFilteredEvents(query));
 	}
 
-	@DeleteMapping(value = DELETE_EVENT_URL)
+	@DeleteMapping(DELETE_EVENT_URL)
 	@Override
 	public void deleteEvent(@PathVariable final Long id) {
 		eventService.delete(id);
 	}
 
-	@PutMapping(value = UPDATE_EVENT_URL)
+	@PutMapping(UPDATE_EVENT_URL)
 	@Override
 	public void updateEvent(@PathVariable Long id, @RequestBody final UpdateEventParameter event) {
 		eventService.update(
